6689872626c8c4fd08e2c0c24779f503043132ba
svnldwg <s.ludwig.g@gmail.com>
2021-01-23 19:32:24 +0100

    display tronald dump quote also on http call

--- lambda/http.php
+++ lambda/http.php
@@ -2,6 +2,13 @@
 
 declare(strict_types=1);
 
+use LambdaPhpSkeleton\TronaldDump;
+
 require_once __DIR__ . '/../config/bootstrap.php';
 
-echo "<h1>Hello world!</h1>";
+echo '<h1>Hello world! Here is your Tronald Dump quote:</h1>';
+
+$client = new TronaldDump\Client(new \Bref\Logger\StderrLogger()); // @TODO use DI
+$quote = $client->getRandomQuote();
+
+echo '<cite>' . $quote['value'] . '</cite>';
